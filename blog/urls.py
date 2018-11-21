9c1c857203fa502a9bfeb8dd06218b9cd0a09186
songhongxing <602467473@qq.com>
2018-11-21 18:52:15 +0800

    增删改查

---
+++ blog/urls.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+# @Time   : 2018/11/21 1:30 PM
+# @Author : songhx
+# @File   : urls.py
+
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'blog'
+urlpatterns = [
+    #正则表达式^开始$结束，
+    url(r'^index/$', views.index),
+    url(r'^article/(?P<article_id>[0-9]+)$', views.article_page, name='article_page'),
+    url(r'^edit/(?P<article_id>[0-9]+)$', views.edit_page, name='edit_page'),
+    url(r'^edit/action/$', views.edit_action, name='edit_action'),
+]
